e03ba868c57935e81b00dcc405c48f1cad7546bf
jshe857 <jeffrey_shen88@hotmail.com>
2015-10-05 02:06:28 +1100

    corrected variance plot

--- results.py
+++ results.py
@@ -167,10 +167,9 @@ rng = range(n*spk_samples+100,(n+1)*spk_samples)
 m2 = m2[[x-100 for x in rng]]-0.12
 v2 = v2[[x-100 for x in rng]]
 plt.figure()
-plt.plot(X_dev2[rng,TIME_IDX],y_dev2[rng],'g-',alpha=0.5,label="Ground Truth" )
+plt.plot(X_dev2[rng,TIME_IDX],y_dev2[rng],'g-',alpha=0.7,label="Ground Truth" )
 plt.plot(X_dev2[rng,TIME_IDX],m2,'b-',alpha=0.5,label="EP" )
-plt.plot(X_dev2[rng,TIME_IDX],m2+2*np.sqrt(v2),'b-',alpha=0.1 )
-plt.plot(X_dev2[rng,TIME_IDX],m2-2*np.sqrt(v2),'b-',alpha=0.1 )
+plt.fill_between(X_dev2[rng,TIME_IDX],m2+5*np.sqrt(v2),m2-5*np.sqrt(v2),alpha=0.25)
 
 plt.legend()
 
